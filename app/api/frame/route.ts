da70548d2dcc17d33aff0a879831e0cf3b7aa42d
Pankaj Kumar <pankajkumardas727@gmail.com>
2024-01-31 21:03:02 +0530

    added params

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -124,7 +124,9 @@ export async function GET(req: NextRequest): Promise<Response> {
   const APP_URL = "https://frames-lenspost.vercel.app";
 
   const searchParams = req.nextUrl.searchParams;
-  const imageUrl = searchParams.get("image") || "";
+  const imageUrl =
+    searchParams.get("image") ||
+    "https://frames-lenspost.vercel.app/api/frame?image=https://images.unsplash.com/photo-1683009427513-28e163402d16?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&tokenUri=%22wow";
   const tokenUri = searchParams.get("tokenUri") || "";
 
   return new NextResponse(`
