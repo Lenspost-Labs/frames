f8651bd90eaee8d308f73265d190441cd5f831b7
Pankaj Kumar <pankajkumardas727@gmail.com>
2024-02-02 23:20:20 +0530

    add: redirect to the app

--- app/page.tsx
+++ app/page.tsx
@@ -37,7 +37,7 @@ const Home: NextPage = () => {
     <main className="flex min-h-screen flex-col items-center justify-center gap-12 p-24">
       <h1 className="font-bold text-5xl">Frames Lenspost</h1>
       <a
-        href="https://lenspost.xyz"
+        href="https://app.lenspost.xyz"
         className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
         target="_blank"
         rel="noopener noreferrer"
