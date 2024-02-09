e61f4ff6ca2099339b638c53cc7aed966c776fed
Pankaj Kumar <pankajkumardas727@gmail.com>
2024-02-10 01:20:49 +0530

    update6

--- contract/contract.sol
+++ contract/contract.sol
@@ -1,12 +1,11 @@
 // SPDX-License-Identifier: MIT
-// Deployed to 0xC26fBA5C9C30AD5a926745B9E0966e13BBc56CFD
 
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract OnChainCow is ERC721URIStorage {
+contract FramesLenspost is ERC721URIStorage {
     uint256 private currentTokenId = 0;
 
     constructor() ERC721("Frames Lenspost", "FLP") {}
