f1bf953c43baf20c5826223035becfd99abbaca5
Pankaj Kumar <pankajkumardas727@gmail.com>
2024-03-25 00:37:42 +0530

    metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,5 @@
 import { NextPage } from "next";
-import type { Metadata } from "next";
-import { config } from "@/config/config";
 import { Default } from "@/components";
-
-export const metadata: Metadata = {
-  title: "Frames Lenspost",
-  description: "Share farcater frames from Lenspost",
-  icons: [
-    "/favicon.ico",
-  ],
-  metadataBase: new URL(config?.APP_URL || ""),
-};
 
 const Home: NextPage = () => {
   return <Default text="Frames Lenspost" />;
