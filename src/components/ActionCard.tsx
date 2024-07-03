c981457134e1304b7ce5dfc556ab37247a658af8
Pankaj Kumar <pankajkumardas727@gmail.com>
2024-07-03 18:30:06 +0530

    update: action page

---
+++ src/components/ActionCard.tsx
@@ -0,0 +1,20 @@
+import { FrameData } from '@/types';
+import Image from 'next/image';
+import { FC } from 'react';
+
+const ActionCard: FC<FrameData> = ({ imageUrl }) => {
+  return (
+    <div className="mx-auto flex max-w-4xl flex-col justify-between gap-8 rounded-3xl bg-white p-6 shadow-2xl sm:flex-row sm:p-10">
+      <Image
+        className="w-full shadow-xl "
+        priority={true}
+        src={imageUrl}
+        height={1080}
+        width={1920}
+        alt="image"
+      />
+    </div>
+  );
+};
+
+export default ActionCard;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
+export { default as ActionCard } from './ActionCard';
 export { default as FrameCard } from './FrameCard';
 export { default as Default } from './Default';
