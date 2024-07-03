8735021110c29b547f86a05c9995e2d0e66c308f
Pankaj Kumar <pankajkumardas727@gmail.com>
2024-07-03 18:41:56 +0530

    fix: image

--- src/components/ActionCard.tsx
+++ src/components/ActionCard.tsx
@@ -6,7 +6,7 @@ const ActionCard: FC<FrameData> = ({ imageUrl }) => {
   return (
     <div className="mx-auto flex max-w-4xl flex-col justify-between gap-8 rounded-3xl bg-white p-6 shadow-2xl sm:flex-row sm:p-10">
       <Image
-        className="w-full shadow-xl "
+        className="w-full rounded-3xl shadow-xl "
         priority={true}
         src={imageUrl}
         height={1080}
