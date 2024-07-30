afeb64f12f1c47a61ea0902030a8ca6e232c61f7
Pankaj Kumar <pankajkumardas727@gmail.com>
2024-07-30 23:18:52 +0530

    test4

--- src/app/api/composer/[route]/route.ts
+++ src/app/api/composer/[route]/route.ts
@@ -2,7 +2,7 @@ import { FrameRequest } from '@coinbase/onchainkit/frame';
 import { NextResponse, NextRequest } from 'next/server';
 
 const handler = async (req: NextRequest): Promise<NextResponse> => {
-  const appUrl = 'https://8aa2-103-76-139-15.ngrok-free.app';
+  const appUrl = 'https://frontend-ca4b98j70-raveshare.vercel.app';
 
   const body: FrameRequest = await req.json();
   const messageBytes = body?.trustedData?.messageBytes;
