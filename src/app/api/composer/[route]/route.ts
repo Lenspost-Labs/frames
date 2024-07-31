05520fc2edba87234b77afe42c48ea6315d477e9
Pankaj Kumar <pankajkumardas727@gmail.com>
2024-07-31 12:22:48 +0530

    test: testing sponsor frames

--- src/app/api/composer/[route]/route.ts
+++ src/app/api/composer/[route]/route.ts
@@ -1,12 +1,13 @@
 import { FrameRequest } from '@coinbase/onchainkit/frame';
 import { NextResponse, NextRequest } from 'next/server';
-import { LENSPOST_APP_URL } from '@/data';
+// import { LENSPOST_APP_URL } from '@/data';
 
 const handler = async (req: NextRequest): Promise<NextResponse> => {
   const body: FrameRequest = await req.json();
   const messageBytes = body?.trustedData?.messageBytes;
   const url = decodeURIComponent(
-    LENSPOST_APP_URL + `/?actionType=composer&fc-auth=FC ${messageBytes}`
+    'https://8aa2-103-76-139-15.ngrok-free.app' +
+      `/?actionType=composer&fc-auth=FC ${messageBytes}`
   );
 
   const actionMetadata = {
